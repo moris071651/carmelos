3ebb502e2dbe3aeb63f101b856a43bbb7d34a7be
Николай Вълканов <93319862+CooDiiNgg@users.noreply.github.com>
2024-06-20 17:26:27 +0300

    UPDATE: added some server functions

--- src/server/main.c
+++ src/server/main.c
@@ -6,6 +6,7 @@
 #include "AES-PCBC.h"
 #include "fileStorage.h"
 #include "CRC.h"
+#include "socket_work.h"
 
 
 void encryptFileContent(File *file, AES_PCBC *aes_pcbc) {
@@ -58,99 +59,46 @@ void login(SQLite *sqlite, char *username, char *password, bool *success) {
     *success = correct;
 }
 
-int main(int argc, char* arv[]) {
-    AES_PCBC aes_pcbc;
-    AES_PCBC_Data key;
-    AES_PCBC_Data iv;
-    unsigned char data[] = "Hello World! Hello World!aass";
-    unsigned int data_len = strlen(data);
-
-    key.data = (unsigned char*)"1234567890123456";
-    key.data_len = 16;
-
-    iv.data = (unsigned char*)"1234567890123456";
-    iv.data_len = 16;
-
-    AES_PCBC_Setup(&aes_pcbc, &key, &iv, 0);
-    AES_PCBC_Encrypt(&aes_pcbc, data, data_len);
-    printf("Encrypted: %s\n", data);
-    data_len = data_len % 16 == 0 ? data_len : data_len + (16 - data_len % 16);
-    AES_PCBC_Decrypt(&aes_pcbc, data, data_len);
-    printf("Decrypted: %s\n", data);
-
-    // Test_parts();
-
-    // SQLite sqlite;
-    // SQLite_Open(&sqlite, "test.db");
-    // SQLite_Execute(&sqlite, "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)", NULL, NULL);
-
-    // bool exists;
-    // SQLite_UserExists(&sqlite, "admin", &exists);
-    // if (!exists) {
-    //     SQLite_UserInsert(&sqlite, "admin", "admin");
-    // }
-
-    // SQLite_Close(&sqlite);
-
-    // test file storage
-
-    initFileStorage();
-
-    File file;
-    file.username = "admin";
-    file.timestamp = time(NULL);
-    file.filename = "Test_Note";
-    file.content = malloc(30);
-    strcpy(file.content, "Hello World! Hello World!");
-    file.size = strlen(file.content);
-
-
-    //convert content to char array
-
-    AES_PCBC_Setup(&aes_pcbc, &key, &iv, 0);
-
-
-    // size_t size = file.size % 16 == 0 ? file.size : file.size + (16 - file.size % 16);
-    // char content_tmp[size + 1];
-    // strcpy(content_tmp, file.content);
-    // content_tmp[file.size] = '\0';
-    // AES_PCBC_Encrypt(&aes_pcbc, content_tmp, file.size);
-    // file.content = realloc(file.content, size);
-    // strcpy(file.content, content_tmp);
-    // file.size = size;
-
-    encryptFileContent(&file, &aes_pcbc);
-    printf("Encrypted content: %s\n", file.content);
-
-    //I should really fix this mess....
-    // Ig later cause rn i'll cry if i see this again
-
-    saveFile(&file);
-
-    FileMeta *meta;
-    int count;
-
-    listFiles("admin", &meta, &count);
-
-    for (int i = 0; i < count; i++) {
-        printf("File: %s\n", meta[i].filename);
+void Call_Functions(SQLite *sqlite, Socket *sock, char *title, AES_PCBC *aes_pcbc) {
+    // check every possible call and call full_function
+    if(strcmp(title, "signUp") == 0) {
+        full_signUp(sqlite, sock, aes_pcbc);
     }
+    else if(strcmp(title, "login") == 0) {
+        full_login(sqlite, sock, aes_pcbc);
+    }
+    else if(aes_pcbc->key[0] == '\0' || aes_pcbc->iv[0] == '\0') {
+        Response response;
+        response.success = false;
+        strcpy(response.message, "You need to login first");
+        Socket_SendResponse(sock, &response);
+    }
+    else {
+        Response response;
+        response.success = false;
+        strcpy(response.message, "Invalid title");
+        Socket_SendResponse(sock, &response);
+    }
+}
 
-    FileContent content;
-    getFile(meta[0].id, &content);
+// main
 
+int main(int argc, char *argv[]) {
+    SQLite sqlite;
+    initAll(&sqlite);
 
-    // content.size = content.size % 16 == 0 ? content.size : content.size + (16 - content.size % 16);
-    // char content_tmp2[content.size + 1];
-    // strcpy(content_tmp2, content.content);
-    // content_tmp2[content.size] = '\0';
-    // AES_PCBC_Decrypt(&aes_pcbc, content_tmp2, content.size);
-    // content.content = realloc(content.content, content.size);
-    // strcpy(content.content, content_tmp2);
-
-    decryptFileContent(&content, &aes_pcbc);
-
-    printf("File content: %s\n", content.content);
-
-    deleteFile(meta[0].id);
-};
+    //wait for connection
+    Socket sock;
+    Socket_Init(&sock);
+    Socket_Open(&sock, 1337);
+    while(1){
+        if(sock.connected){
+            break;
+        }
+    }
+    AES_PCBC aes_pcbc;
+    AES_PCBC_Init(&aes_pcbc);
+    while(1){
+        Socket_Wait(&sock);
+    }
+}
