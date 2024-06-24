ddbe3d08f2499215bedf9d49f8b1918ff810ab74
Николай Вълканов <93319862+CooDiiNgg@users.noreply.github.com>
2024-06-24 16:10:28 +0300

    NEW: Socket types

---
+++ src/client/socketTypes.h
@@ -0,0 +1,49 @@
+#ifndef SOCKETTYPES_H
+#define SOCKETTYPES_H
+
+#include <stdbool.h>
+#include <time.h>
+
+
+
+typedef struct {
+    int type;
+    char username[256];
+    char password[256];
+} User;
+
+typedef struct {
+    int type;
+    char id[512];
+    char filename[256];
+    size_t size;
+} FileSocket;
+
+typedef struct {
+    int type;
+    char id[512];
+    char filename[256];
+    time_t timestamp;
+} FileMeta_Socket;
+
+typedef struct {
+    int type;
+    char username[256];
+} userResponse;
+
+typedef union {
+    int type;
+    User login;
+    User signup;
+    userResponse response;
+    FileSocket newItem;
+    FileMeta_Socket newItem_response;
+    FileMeta_Socket delItem;
+    FileMeta_Socket delItem_response;
+    FileMeta_Socket getItem;
+    FileSocket getItem_response;
+    FileSocket updateItem;
+    FileMeta_Socket updateItem_response;
+} AllData;
+
+#endif
