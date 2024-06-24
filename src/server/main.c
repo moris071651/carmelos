f9b14bf89c2ca06ab0184e3ec17eb288b8ae69ba
Николай Вълканов <93319862+CooDiiNgg@users.noreply.github.com>
2024-06-24 19:41:37 +0300

    UPDATE: Starting to build main

--- src/server/main.c
+++ src/server/main.c
@@ -39,6 +39,14 @@ void hashPassword(char *password, char *hash) {
     sprintf(hash, "%u", crc32);
 }
 
+void hashID(char *id, char *hash) {
+    CRC crc;
+    CRC_Init(&crc);
+    CRC_Update(&crc, id, strlen(id));
+    unsigned int crc32 = CRC_Final(&crc);
+    sprintf(hash, "%u", crc32);
+}
+
 void initAll(SQLite *sqlite) {
     SQLite_Open(sqlite, "test.db");
     SQLite_Execute(sqlite, "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)", NULL, NULL);
@@ -61,54 +69,106 @@ void login(SQLite *sqlite, char *username, char *password, bool *success) {
 
 // full functions
 
-void full_signUp(SQLite *sqlite, Socket *sock) {
-    User user;
-    Socket_RecieveUser(sock, &user);
-    signUp(sqlite, user.username, user.password);
-    Response response;
-    response.success = true;
-    strcpy(response.message, "Signed up successfully");
-    Socket_SendResponse(sock, &response);
-}
-
-void full_login(SQLite *sqlite, Socket *sock, AES_PCBC *aes_pcbc, AES_PCBC_Data *key) {
-    User user;
-    Socket_RecieveUser(sock, &user);
-    bool success;
-    login(sqlite, user.username, user.password, &success);
-    Response response;
-    response.success = success;
-    if(success) {
-        strcpy(response.message, "Logged in successfully");
-        key->data = user.password;
-        key->data_len = strlen(user.password);
+void full_signUp(AllData *data, SQLite *sqlite, Socket *sock, AES_PCBC *aes_pcbc, AES_PCBC_Data *key) {
+    char username[256];
+    char password[256];
+    strcpy(username, data->signup.username);
+    strcpy(password, data->signup.password);
+    hashPassword(password, password);
+    int success = 0;
+    login(sqlite, username, password, &success);
+    if (success) {
+        userResponse response;
+        response.type = 3;
+        response.username[0] = '\0';
+        Socket_SendResponse(sock, &response);
+    } else {
+        signUp(sqlite, username, password);
+        userResponse response;
+        response.type = 3;
+        strcpy(response.username, username);
+        Socket_SendResponse(sock, &response);
+        strcpy(key->data, data->signup.password);
+        key->data_len = strlen(data->signup.password);
         AES_PCBC_Setup(aes_pcbc, key, key, 1);
     }
-    else {
-        strcpy(response.message, "Invalid username or password");
-    }
-    Socket_SendResponse(sock, &response);
 }
 
-void Call_Functions(SQLite *sqlite, Socket *sock, char *title, AES_PCBC *aes_pcbc, AES_PCBC_Data *key) {
-    // check every possible call and call full_function
-    if(strcmp(title, "signUp") == 0) {
-        full_signUp(sqlite, sock);
-    }
-    else if(strcmp(title, "login") == 0) {
-        full_login(sqlite, sock, aes_pcbc, key);
-    }
-    else if(aes_pcbc->key[0] == '\0' || aes_pcbc->iv[0] == '\0') {
-        Response response;
-        response.success = false;
-        strcpy(response.message, "You need to login first");
+void full_login(AllData *data, SQLite *sqlite, Socket *sock, AES_PCBC *aes_pcbc, AES_PCBC_Data *key) {
+    char username[256];
+    char password[256];
+    strcpy(username, data->login.username);
+    strcpy(password, data->login.password);
+    hashPassword(password, password);
+    int success = 0;
+    login(sqlite, username, password, &success);
+    if (success) {
+        userResponse response;
+        response.type = 3;
+        strcpy(response.username, username);
+        Socket_SendResponse(sock, &response);
+        strcpy(key->data, data->login.password);
+        key->data_len = strlen(data->login.password);
+        AES_PCBC_Setup(aes_pcbc, key, key, 1);
+    } else {
+        userResponse response;
+        response.type = 3;
+        response.username[0] = '\0';
         Socket_SendResponse(sock, &response);
     }
-    else {
-        Response response;
-        response.success = false;
-        strcpy(response.message, "Invalid title");
-        Socket_SendResponse(sock, &response);
+}
+
+void full_newItem(AllData *data, Socket *sock, AES_PCBC *aes_pcbc, AES_PCBC_Data *key) {
+    File file;
+    strcpy(file.filename, data->newItem.filename);
+    time_t timestamp;
+    time(&timestamp);
+    file.timestamp = timestamp;
+    file.size = data->newItem.size;
+    if (file.size > 0) {
+        file.content = malloc(file.size);
+        Socket_RecieveContent(sock, file.content, file.size);
+        int count = 0;
+        char count_str[16];
+        char tmp_id[512];
+        strcpy(tmp_id, data->newItem.filename);
+        char tmp_timestamp[32];
+        sprintf(tmp_timestamp, "%ld", file.timestamp);
+        strcat(tmp_id, tmp_timestamp);
+        hashID(tmp_id, count_str);
+        count = atoi(count_str);
+        AES_PCBC_Setup(aes_pcbc, key, key, count);
+        encryptFileContent(&file, aes_pcbc);        
+    } else {
+        file.content = malloc(1);
+        file.content[0] = '\0';
+    }
+    saveFile(&file);
+
+    FileMeta_Socket meta;
+    meta.type = 5;
+
+    generate_id(&file, meta.id);
+    strcpy(meta.filename, file.filename);
+    meta.timestamp = file.timestamp;
+    Socket_SendFileMeta(sock, &meta, 5);
+}
+
+
+
+void Call_Functions(SQLite *sqlite, Socket *sock, int type, AES_PCBC *aes_pcbc, AES_PCBC_Data *key, AllData *data) {
+    switch (type) {
+        case 1:
+            full_login(data, sqlite, sock, aes_pcbc, key);
+            break;
+        case 2:
+            full_signUp(data, sqlite, sock, aes_pcbc, key);
+            break;
+        case 4:
+            full_newItem(data, sock, aes_pcbc, key);
+            break;
+        default:
+            break;
     }
 }
 
@@ -131,9 +191,8 @@ int main(int argc, char *argv[]) {
     AES_PCBC_Data key;
     AES_PCBC_Init(&aes_pcbc);
     while(1){
-        Socket_Wait(&sock);
-        char title[256];
-        Socket_RecieveTitle(&sock, title);
-        Call_Functions(&sqlite, &sock, title, &aes_pcbc, &key);
+        AllData data;
+        Socket_Recieve(&sock, &data);
+        Call_Functions(&sqlite, &sock, data.type, &aes_pcbc, &key, &data);
     }
 }
