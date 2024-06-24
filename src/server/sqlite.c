20e5d6159364f9503de4471ea8f6fce958b484fc
Николай Вълканов <93319862+CooDiiNgg@users.noreply.github.com>
2024-06-25 00:40:06 +0300

    FIX: Make it work...

--- src/server/sqlite.c
+++ src/server/sqlite.c
@@ -12,14 +12,22 @@ void SQLite_Execute(SQLite *sqlite, const char *sql, int (*callback)(void*,int,c
     sqlite3_exec(sqlite->db, sql, callback, data, &sqlite->err_msg);
 }
 
+static int callback(void *data, int argc, char **argv, char **azColName) {
+    bool *exists = (bool *) data;
+    *exists = true;
+    return 0;
+}
+
+static int callback2(void *data, int argc, char **argv, char **azColName) {
+    bool *correct = (bool *) data;
+    *correct = true;
+    return 0;
+}
+
 void SQLite_UserExists(SQLite *sqlite, const char *username, bool *exists) {
     char sql[512];
     sprintf(sql, "SELECT * FROM users WHERE username = '%s'", username);
-    if (sqlite3_exec(sqlite->db, sql, NULL, NULL, &sqlite->err_msg) != SQLITE_OK) {
-        *exists = false;
-    } else {
-        *exists = true;
-    }
+    sqlite3_exec(sqlite->db, sql, callback, exists, &sqlite->err_msg);
 }
 
 void SQLite_UserInsert(SQLite *sqlite, const char *username, const char *password) {
@@ -31,9 +39,5 @@ void SQLite_UserInsert(SQLite *sqlite, const char *username, const char *passwor
 void SQLite_UserCheck(SQLite *sqlite, const char *username, const char *password, bool *correct) {
     char sql[512];
     sprintf(sql, "SELECT * FROM users WHERE username = '%s' AND password = '%s'", username, password);
-    if (sqlite3_exec(sqlite->db, sql, NULL, NULL, &sqlite->err_msg) != SQLITE_OK) {
-        *correct = false;
-    } else {
-        *correct = true;
-    }
+    sqlite3_exec(sqlite->db, sql, callback2, correct, &sqlite->err_msg);
 }
